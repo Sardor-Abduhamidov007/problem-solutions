7792d9f5e0f3f063f3765c7f335177abc33e99aa
Sardor-Abduhamidov007 <sardorabduhamidov007@gmail.com>
2022-12-21 22:19:47 +0500

    initial commit

--- codewars/script.js
+++ codewars/script.js
@@ -289,3 +289,19 @@ function sum(number) {
   }
 }
 console.log(sum(4560));
+
+// Write a function that takes a string of parentheses, and determines if the order of the
+// parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+
+// Examples
+// "()"              =>  true
+// ")(()))"          =>  false
+// "("               =>  false
+// "(())((()())())"  =>  true
+
+function doTest(braces){
+    while(/\(\)/g.test(braces)){braces = braces.replace(/\(\)/g,"")}
+    return !braces.length;
+}
+
+console.log(doTest("(())((()())())"))
